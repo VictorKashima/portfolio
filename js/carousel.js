05dd663deb886116ed7594288d66734b46950a8c
victorkashima <victorkashima04@gmail.com>
2023-07-26 19:44:51 -0300

    FIX: FIX CAROUSEL
    ERROR: As I'm using two carousel, I need instance two differents Swiper on script file

--- js/carousel.js
+++ js/carousel.js
@@ -1,68 +1,76 @@
-// const swiper = new Swiper('.swiper', {
+const swiperOne = new Swiper('.first-carousel', {
 
-//     a11y: {
-//       prevSlideMessage: 'Previous slide',
-//       nextSlideMessage: 'Next slide',
-//     },
+    loop: false,
+    speed: 400,
 
-//     loop: false,
-//     speed: 400,
+    slidesPerView: 1.5,
+    spaceBetween: 20,
+    slidesPerGroup: 1,
+    rewind: true,
 
-//     slidesPerView: 1.5,
-//     spaceBetween: 20,
-//     slidesPerGroup: 1,
-//     rewind: true,
+    breakpoints: {
 
-//     breakpoints: {
+        768: {
+          slidesPerView: 1.6,
+          spaceBetween: 40
+        },
 
-//         768: {
-//           slidesPerView: 1.6,
-//           spaceBetween: 40
-//         },
-
-//         1000: {
-//           slidesPerView: 2,
-//           spaceBetween: 40
-//         },
+        1000: {
+          slidesPerView: 2,
+          spaceBetween: 40
+        },
         
-//         1368: {
-//           slidesPerView: 2.7,
-//           spaceBetween: 40
-//         },
+        1368: {
+          slidesPerView: 2.7,
+          spaceBetween: 40
+        },
 
-//     },
+    },
 
-// })
+});
 
-// function vaicarai() {
-//   console.log("QDWKOQWODQL");
-//   swiper.slideNext( );
-// }
-
-const swiper = new Swiper('.swiper', {
-
-  cssMode: true,
-  loop: false,
-  speed: 400,
-  allowSlideNext: true,
-  allowSlidePrev: true,
-
-  navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-  },
-
-  slidesPerView: 2.7,
-  slidesPerGroup: 1,
-  spaceBetween: 40,
-  rewind: true,
-
-})
-
-function previousSlide() {
-  swiper.slidePrev();
+function firstPreviousSlide() {
+  swiperOne.slidePrev();
 }
 
-function nextSlide() {
-  swiper.slideNext();
+function firstNextSlide() {
+  swiperOne.slideNext();
+}
+
+const swiperTwo = new Swiper('.second-carousel', {
+
+  loop: false,
+  speed: 400,
+  slidesPerView: 1.5,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  rewind: true,
+
+  breakpoints: {
+
+      768: {
+        slidesPerView: 1.6,
+        spaceBetween: 40
+      },
+
+      1000: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      
+      1368: {
+        slidesPerView: 2.7,
+        spaceBetween: 40
+      },
+
+  },
+
+});
+
+function secondPreviousSlide() {
+  swiperTwo.slidePrev();
+}
+
+function secondNextSlide() {
+  swiperTwo.slideNext();
 }
