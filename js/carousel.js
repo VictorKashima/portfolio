6899fc257d36103ca56deaad7503e16fb43b731a
victorkashima <victorkashima04@gmail.com>
2023-07-31 14:51:42 -0300

    FIX: Try fix carousel in mobile devices

--- js/carousel.js
+++ js/carousel.js
@@ -1,31 +1,32 @@
 const swiperOne = new Swiper('#first-carousel', {
 
-    // loop: false,
-    // speed: 500,
+    loop: false,
+    speed: 500,
 
-    // slidesPerView: 1.1,
-    // spaceBetween: 20,
-    // slidesPerGroup: 1,
-    // rewind: true,
+    slidesPerView: 1.1,
+    spaceBetween: 20,
+    slidesPerGroup: 1,
+    rewind: true,
+    cssMode: true,
 
-    // breakpoints: {
+    breakpoints: {
 
-    //     768: {
-    //       slidesPerView: 1.6,
-    //       spaceBetween: 40
-    //     },
+        768: {
+          slidesPerView: 1.6,
+          spaceBetween: 40
+        },
 
-    //     1000: {
-    //       slidesPerView: 2,
-    //       spaceBetween: 40
-    //     },
+        1000: {
+          slidesPerView: 2,
+          spaceBetween: 40
+        },
         
-    //     1368: {
-    //       slidesPerView: 2.7,
-    //       spaceBetween: 40
-    //     },
+        1368: {
+          slidesPerView: 2.7,
+          spaceBetween: 40
+        },
 
-    // },
+    },
 
 });
 
@@ -39,31 +40,32 @@ function firstNextSlide() {
 
 const swiperTwo = new Swiper('#second-carousel', {
 
-  // loop: false,
-  // speed: 500,
-  // slidesPerView: 1.1,
-  // spaceBetween: 20,
-  // slidesPerGroup: 1,
-  // rewind: true,
+  loop: false,
+  speed: 500,
+  slidesPerView: 1.1,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  rewind: true,
+  cssMode: true,
 
-  // breakpoints: {
+  breakpoints: {
 
-  //     768: {
-  //       slidesPerView: 1.6,
-  //       spaceBetween: 40
-  //     },
+      768: {
+        slidesPerView: 1.6,
+        spaceBetween: 40
+      },
 
-  //     1000: {
-  //       slidesPerView: 2,
-  //       spaceBetween: 40
-  //     },
+      1000: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
       
-  //     1368: {
-  //       slidesPerView: 2.7,
-  //       spaceBetween: 40
-  //     },
+      1368: {
+        slidesPerView: 2.7,
+        spaceBetween: 40
+      },
 
-  // },
+  },
 
 });
 
