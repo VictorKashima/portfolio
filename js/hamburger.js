0734cf7190f2180667dff1ea9e1e75d2770ee330
victorkashima <victorkashima04@gmail.com>
2023-08-15 21:51:07 -0300

    FIX: Mobile side bar

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,6 +2,16 @@ document.querySelector(".hamburger").addEventListener("click", () =>
     document.querySelector(".main-header").classList.toggle("show-menu")
 );
 
+var links = document.querySelectorAll(".mobile-link")
+
+links.forEach(function(link) {
+  
+  link.addEventListener("click", () =>
+      document.querySelector(".main-header").classList.remove("show-menu")
+  );
+
+});
+
 function addClassOnWidthChange() {
   const addWrappers = document.querySelectorAll('.portfolio-content');
   if (window.innerWidth <= 768) {
